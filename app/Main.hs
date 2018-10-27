2d452688379a38db5fb5eb628857419316d254e3
jannicbeck <jannicbeck@gmail.com>
2018-10-27 15:30:46 +0200

    Add json response and routes

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Network.Wai
-import Network.HTTP.Types
+import Network.Wai (responseLBS, Application, pathInfo)
+import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
+import Data.Aeson (encode)
+import Data.Text (Text)
 
+
+-- somehow putStrLn is executed twice when visiting a route
 app :: Application
-app _ respond = do
+app req res = do
     putStrLn "I've done some IO here"
-    respond $ responseLBS
+    res $
+      case pathInfo req of
+        ["hello"] -> helloRoute
+        _ -> anyRoute
+
+route = responseLBS
         status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+        [("Content-Type", "application/json")]
+
+anyRoute = route (encode ("Hello there" :: Text))
+helloRoute = route (encode ("Hello World" :: Text))
 
 main :: IO ()
 main = do
-    putStrLn "http://localhost:8080/"
-    run 8080 app
+  let port = 3000
+  putStrLn $ "Listening on port " ++ show port
+  run port app
