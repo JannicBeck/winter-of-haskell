08fa0ec03549508de25274d3ba66cfd22e752a7a
jannicbeck <jannicbeck@gmail.com>
2018-10-31 11:48:38 +0100

    Add groups

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,15 +5,17 @@ module Lib
     ) where
 
 import           Data.Aeson
+import qualified Data.Set                 as Set
 import           Data.Text                (Text)
+import           Data.Time                (ZonedTime)
 import           GHC.Generics
 import           Network.HTTP.Types       (status200)
 import           Network.Wai              (Application, pathInfo, responseLBS)
 import           Network.Wai.Handler.Warp (run)
 
 
-data User = User { name :: Text, email :: Text }
-          deriving (Generic, Show)
+data User = User { userName :: Text, userEmail :: Text }
+          deriving (Generic, Show, Eq, Ord)
 
 instance ToJSON User where
   -- No need to provide a toJSON implementation.
@@ -25,7 +27,20 @@ instance ToJSON User where
 instance FromJSON User
   -- No need to provide a parseJSON implementation.
 
-user = User { name = "Jannic Beck", email = "jannicbeck@gmail.com" }
+data Group = Group { groupName :: Text, groupMembers :: Set.Set User }
+           deriving (Generic, Show)
+data GroupOptions = GroupOptions { giftCostLimit :: Maybe Text, dateOfDrawing :: Maybe ZonedTime }
+                  deriving (Generic, Show)
+
+instance ToJSON Group where
+  toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON Group
+
+jannic = User { userName = "Jannic Beck", userEmail = "jannicbeck@gmail.com" }
+nico = User { userName = "Nicolas Beck", userEmail = "nico1510@gmail.com" }
+
+g = Group { groupName = "Christmas", groupMembers = Set.fromList([jannic, nico]) }
 
 -- somehow putStrLn is executed twice when visiting a route
 app :: Application
@@ -33,7 +48,8 @@ app req res = do
     putStrLn "I've done some IO here"
     res $
       case pathInfo req of
-        ["api"]    -> secretSantaRoute
+        ["users"]  -> usersRoute
+        ["groups"] -> groupsRoute
         ["health"] -> healthRoute
         _          -> anyRoute
 
@@ -42,7 +58,8 @@ route = responseLBS
         [("Content-Type", "application/json")]
 
 anyRoute = route (encode ("Welcome to Secret Santa!" :: Text))
-secretSantaRoute = route (encode user)
+usersRoute = route (encode jannic)
+groupsRoute = route (encode g)
 healthRoute = route (encode ("I'm fine" :: Text))
 
 listen :: IO ()
