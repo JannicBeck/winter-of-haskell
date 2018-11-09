7ecc6fd3c0fc2214cab2c8532d6390590a6f98d2
nico1510 <Nicolas-Beck@gmx.de>
2018-11-09 21:48:57 +0100

    added simple schema https://github.com/JannicBeck/winter-of-haskell/issues/4

--- Makefile
+++ Makefile
@@ -6,8 +6,12 @@ run:
 
 db:
 	(docker inspect --type=image $(imageName)) || (docker build . -t $(imageName))
-	(docker start $(containerName)) || (docker run --name $(containerName) -d -p 5432:5432 $(imageName))
+	(docker start $(containerName)) || (docker run --name $(containerName) -p 5432:5432 $(imageName))
 	docker ps -a --filter "name=$(containerName)" --format "{{.Names}}: {{.Status}}"
+
+psql:
+	docker start $(containerName)
+	docker exec -ti $(containerName) psql -d winter-db -U winter
 
 kill:
 	docker stop $$(docker ps -a -q); docker rm $$(docker ps -a -q)
--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -22,3 +22,66 @@ CREATE DATABASE "winter-db"
        LC_CTYPE = 'en_US.utf8'
        CONNECTION LIMIT = -1;
 
+\connect winter-db
+
+DROP SCHEMA IF EXISTS winter;
+CREATE SCHEMA IF NOT EXISTS winter AUTHORIZATION winter;
+
+
+-- -----------------------------------------------------
+-- Table winter.users
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS winter.users;
+
+CREATE TABLE IF NOT EXISTS winter.users (
+  id BIGINT NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  email TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX users_UNIQUE ON winter.users (id ASC);
+CREATE UNIQUE INDEX email_UNIQUE ON winter.users (email ASC);
+
+-- -----------------------------------------------------
+-- Table winter.groups
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS winter.groups;
+
+CREATE TABLE IF NOT EXISTS winter.groups (
+  id BIGINT NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT NULL,
+  cost_limit NUMERIC NULL,
+  creator_id BIGINT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_groups_users_creator
+    FOREIGN KEY (creator_id)
+    REFERENCES winter.users (id)
+);
+
+CREATE UNIQUE INDEX groups_id_UNIQUE ON winter.groups (id ASC);
+CREATE INDEX fk_groups_users_creator_idx ON winter.groups (creator_id ASC);
+
+
+-- -----------------------------------------------------
+-- Table winter.group_members
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS winter.group_members;
+
+CREATE TABLE IF NOT EXISTS winter.group_members (
+  id_group_members BIGINT NOT NULL,
+  group_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  PRIMARY KEY (id_group_members),
+  CONSTRAINT fk_group_members_users
+    FOREIGN KEY (user_id)
+    REFERENCES winter.users (id),
+  CONSTRAINT fk_group_members_groups
+    FOREIGN KEY (group_id)
+    REFERENCES winter.groups (id)
+);
+
+CREATE UNIQUE INDEX group_members_id_UNIQUE ON winter.group_members (id_group_members ASC);
+CREATE INDEX fk_group_members_groups_idx ON winter.group_members (group_id ASC);
+CREATE INDEX fk_group_members_users_idx ON winter.group_members (user_id ASC);
